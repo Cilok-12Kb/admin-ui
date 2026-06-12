3d85d7a0f176cf77c728c989bb6bebb6d2a07f34
Cilok-12Kb <muhammadibadullah27@gmail.com>
2026-06-13 06:31:08 +0700

    Frontend-Backend Integration

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,16 @@ import App from "./App.jsx";
 import "./index.css";
 //import Parent from "./latihan/Parent.jsx";
 //import { CounterContextProvider } from "./context/counterContext.jsx";
+import Form from "./latihan/Form.jsx";
 import { ThemeContextProvider } from "./context/themeContext.jsx";
+import { AuthContextProvider } from "./context/authContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ThemeContextProvider>
-      <App /> 
-    </ThemeContextProvider>
+    <AuthContextProvider>
+      <ThemeContextProvider>
+        <App /> 
+      </ThemeContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
